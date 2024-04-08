43da380ac24a7c02d3bf63dc1f6e75c95f1de0e9
vitormarkis <vitormarkis2369@gmail.com>
2024-04-08 03:39:50 -0300

    fix(web): remove get server side props from admin dashboard page

--- apps/web/src/pages/admin/index.tsx
+++ apps/web/src/pages/admin/index.tsx
@@ -1,47 +1,16 @@
 import { HeaderDashboard } from "@/components/layouts/Header/header-dashboard"
 import { AdminUserListContent } from "@/components/layouts/pages/admin/components/AdminUserListContent"
-import { api } from "@/lib/axios"
-import { getUserSession } from "@/server-fetch/getUserSession"
-import { UserSessionParams } from "@/server-fetch/types"
-import { generateNextCommand } from "@/util/generateNextCommand"
-import { getAuth } from "@clerk/nextjs/server"
-import { GetServerSidePropsContext } from "next"
 import { useSyncExternalStore } from "react"
 
-export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
-  const { getToken, sessionClaims } = getAuth(ctx.req)
-  console.log({ sessionClaims })
-
-  const [error, userResponse] = await getUserSession({ getToken })
-  if (error) throw error
-  const { data, headers } = userResponse
-
-  if (headers["set-cookie"]) {
-    ctx.res.setHeader("set-cookie", headers["set-cookie"])
-  }
-
-  return generateNextCommand({
-    subject: {
-      user: data?.userSession ?? null,
-      serverHeaders: data?.headers ?? null,
-    },
-    options: {
-      shouldShowNotFoundPageWhen({ user }) {
-        return user?.role !== "ADMIN"
-      },
-    },
-  })
-}
-
-export default function AdminDashboard({ serverHeaders }: UserSessionParams) {
-  for (const headerName in serverHeaders) {
-    api.defaults.headers.common[headerName] = serverHeaders[headerName]
-  }
+export default function AdminDashboard() {
+  // for (const headerName in serverHeaders) {
+  //   api.defaults.headers.common[headerName] = serverHeaders[headerName]
+  // }
 
   return (
     <>
       <HeaderDashboard />
-      <div className="mdx:px-8 mx-auto w-full max-w-[1440px] overflow-hidden">
+      <div className="mdx:px-8 mx-auto w-full max-w-[1440px] overflow-hidden pb-24">
         <AdminUserListContent />
       </div>
     </>
