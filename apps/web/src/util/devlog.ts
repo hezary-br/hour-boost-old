d763c482de15abd47de2d27751ed399970fe5550
Vitor Markis <121525239+vitormarkis@users.noreply.github.com>
2024-04-15 20:21:24 +0000

    chore(web): log middleware on prod

--- apps/web/src/util/devlog.ts
+++ apps/web/src/util/devlog.ts
@@ -1,3 +1,5 @@
 export function devlog(...args: any[]) {
-  if (process.env.NODE_ENV === "development") console.log(...args)
+  // if (process.env.NODE_ENV === "development") {
+    console.log(...args)
+  // }
 }
